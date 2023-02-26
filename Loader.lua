3fb1487ce1d719d6d10012ac9c71d6e57968bd3f
hu <109456268+xSxyz@users.noreply.github.com>
2023-02-26 20:02:24 +0700

    Update and rename README.md to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,129 @@
+	require(game:GetService("ReplicatedStorage").Notification).new("God (Cizen Hub)"):Display();
+	wait()
+	setthreadcontext(5)
+	
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	
+	local Player = game:GetService("Players").LocalPlayer
+	
+	local ArgsTransform = {
+		Character = game.Players.LocalPlayer.Character,
+		CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame,
+		Color1 = Color3.fromRGB(166, 0, 0),
+		Color2 = Color3.fromRGB(166, 0, 0),
+		Color3 = Color3.fromRGB(166, 0, 0),
+	}
+	
+	Player.Character.Humanoid:LoadAnimation(ReplicatedStorage.Util.Anims.Storage["2"].RaceTransform):Play()
+	
+	delay(1, function()
+		pcall(function() require(ReplicatedStorage.Effect.Container.RaceTransformation.Main)(ArgsTransform) end)
+	end)
+	
+	setthreadcontext(5)
+	
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	
+	local Player = game:GetService("Players").LocalPlayer
+	
+	local ArgsTransform = {
+		Character = game.Players.LocalPlayer.Character,
+		CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame,
+		Color1 = Color3.fromRGB(166, 0, 111),
+		Color2 = Color3.fromRGB(166, 0, 111),
+		Color3 = Color3.fromRGB(166, 0, 111),
+	}
+	
+	Player.Character.Humanoid:LoadAnimation(ReplicatedStorage.Util.Anims.Storage["2"].RaceTransform):Play()
+	
+	delay(1, function()
+		pcall(function() require(ReplicatedStorage.Effect.Container.RaceTransformation.Main)(ArgsTransform) end)
+	end)
+	
+	setthreadcontext(5)
+	
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	
+	local Player = game:GetService("Players").LocalPlayer
+	
+	local ArgsTransform = {
+		Character = game.Players.LocalPlayer.Character,
+		CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame,
+		Color1 = Color3.fromRGB(155, 155, 155),
+		Color2 = Color3.fromRGB(0, 0, 0),
+		Color3 = Color3.fromRGB(155, 155, 155),
+	}
+	
+	Player.Character.Humanoid:LoadAnimation(ReplicatedStorage.Util.Anims.Storage["2"].RaceTransform):Play()
+	
+	delay(1, function()
+		pcall(function() require(ReplicatedStorage.Effect.Container.RaceTransformation.Main)(ArgsTransform) end)
+	end)
+	
+	setthreadcontext(5)
+	
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	
+	local Player = game:GetService("Players").LocalPlayer
+	
+	local ArgsTransform = {
+
+		Character = game.Players.LocalPlayer.Character,
+		CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame,
+		Color1 = Color3.fromRGB(222, 222, 0),
+		Color2 = Color3.fromRGB(222, 222, 0),
+		Color3 = Color3.fromRGB(222, 222, 0),
+
+	}
+	
+	Player.Character.Humanoid:LoadAnimation(ReplicatedStorage.Util.Anims.Storage["2"].RaceTransform):Play()
+	
+	delay(1, function()
+
+		pcall(function() require(ReplicatedStorage.Effect.Container.RaceTransformation.Main)(ArgsTransform) end)
+	end)
+
+	
+	setthreadcontext(5)
+	
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	
+	local Player = game:GetService("Players").LocalPlayer
+	
+	local ArgsTransform = {
+
+		Character = game.Players.LocalPlayer.Character,
+		CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame,
+		Color1 = Color3.fromRGB(5, 115, 166),
+		Color2 = Color3.fromRGB(5, 115, 166),
+		Color3 = Color3.fromRGB(5, 115, 166),
+
+	}
+	
+	Player.Character.Humanoid:LoadAnimation(ReplicatedStorage.Util.Anims.Storage["2"].RaceTransform):Play()
+	
+	delay(1, function()
+
+		pcall(function() require(ReplicatedStorage.Effect.Container.RaceTransformation.Main)(ArgsTransform) end)
+	end)
+
+	
+	setthreadcontext(5)
+	
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	
+	local Player = game:GetService("Players").LocalPlayer
+	
+	local ArgsTransform = {
+		Character = game.Players.LocalPlayer.Character,
+		CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame,
+		Color1 = Color3.fromRGB(102, 255, 153),
+		Color2 = Color3.fromRGB(102, 255, 153),
+		Color3 = Color3.fromRGB(102, 255, 153),
+	}
+	
+	Player.Character.Humanoid:LoadAnimation(ReplicatedStorage.Util.Anims.Storage["2"].RaceTransform):Play()
+	
+	delay(1, function()
+		pcall(function() require(ReplicatedStorage.Effect.Container.RaceTransformation.Main)(ArgsTransform) end)
+	end)
